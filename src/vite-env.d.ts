9b3dc5c40f3bb5524fb41a1c9f957d8372d59a07
MakerYang <open@wileho.com>
2024-11-15 08:40:49 +0800

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -19,3 +19,6 @@ declare module "*.vue" {
     const component: DefineComponent<{}, {}, any>;
     export default component;
 }
+
+declare const __APP_NAME__: string;
+declare const __APP_VERSION__: string;
